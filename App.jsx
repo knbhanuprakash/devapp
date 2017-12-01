8e6d93ab16dd211e7ca050e7e4485ad3ced5822b
bhanu <bhanu@bhanu-2.local>
2017-12-01 16:59:29 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello hii
+         </div>
+      );
+   }
+}
+export default App;
